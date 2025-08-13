940980bba2c7e9669cd3dd520caa64aabf5b952e
xxx86530-png <xxx86530@gmail.com>
2025-08-13 13:57:59 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,2 +1,1 @@
-next.config.js const nextConfig = { reactStrictMode: true, experimental: { appDir: true } }; module.exports = nextConfig;
-const nextConfig = { reactStrictMode: true, experimental: { appDir: true } }; module.exports = nextConfig;
+ const nextConfig = { reactStrictMode: true, experimental: { appDir: true } }; module.exports = nextConfig;
